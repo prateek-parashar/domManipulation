9631ce75f1ddbd3536593d34570f972bb8a20163
Prateek Parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-07-18 22:54:15 +0530

    Add the error checking, more dom manipulation of the html based on the user input

--- movieProject/assets/scripts/app.js
+++ movieProject/assets/scripts/app.js
@@ -1,9 +1,11 @@
 const addMovieBtn = document.querySelector("header button");
-const addBtn = document.querySelector(".modal__actions .btn--active");
-const cancelBtn = document.querySelector(".modal__actions .btn--passive");
 
 const addMovieScreen = document.getElementById("add-modal");
 const backDrop = document.getElementById("backdrop");
+
+const cancelBtn = addMovieScreen.querySelector(".modal__actions .btn--passive");
+const addBtn = cancelBtn.nextElementSibling;
+const inputArray = addMovieScreen.querySelectorAll("input");
 
 const toggleBackdrop = () => {
     backDrop.classList.toggle("visible");
@@ -11,14 +13,42 @@ const toggleBackdrop = () => {
 
 const toggleAddMovieScreen = () => {
     addMovieScreen.classList.toggle("visible");
+    toggleBackdrop();
 };
 
 addMovieBtn.addEventListener("click", () => {
-    toggleBackdrop();
     toggleAddMovieScreen();
 });
 
-cancelBtn.addEventListener("click", () => {
-    toggleBackdrop();
+const backDropClickHandler = () => {
     toggleAddMovieScreen();
-});
+};
+
+const validateUserInput = (valueArray) => {
+    const name = valueArray[0].value;
+    const url = valueArray[1].value;
+    const rating = valueArray[2].value;
+
+    if (name.trim() === "" || url.trim() === "" || rating.trim() === "") {
+        return false;
+    }
+
+    if (rating < 1 || rating > 5) {
+        return false;
+    }
+
+    return true;
+};
+
+const addBtnHandler = () => {
+    if (validateUserInput(inputArray)) {
+    } else {
+        alert("Bro the input data is invalid");
+    }
+};
+
+backDrop.addEventListener("click", backDropClickHandler);
+
+cancelBtn.addEventListener("click", toggleAddMovieScreen);
+
+addBtn.addEventListener("click", addBtnHandler);
