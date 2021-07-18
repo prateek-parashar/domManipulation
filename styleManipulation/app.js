242bb7c124c6b1ad28882854a3ed9037570d66fd
Prateek Parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-07-18 11:57:35 +0530

    Same code as before but with classList, ought to use classList more so than className property

--- styleManipulation/app.js
+++ styleManipulation/app.js
@@ -3,10 +3,5 @@ const btn = document.querySelector("button");
 const section = document.querySelector("section");
 
 btn.addEventListener("click", () => {
-    if (section.className === "bg-red see-me") {
-        section.className = "john-cena";
-    } else {
-        console.log("HERERE");
-        section.className = "bg-red see-me";
-    }
+    section.classList.toggle("john-cena");
 });
