3a82a0890cee61b98c0874ba5a83d0bfddf1f569
Prateek Parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-07-19 23:37:32 +0530

    Add various methods to manipulate arrays in javascript

---
+++ arrays/app.js
@@ -0,0 +1,100 @@
+/*
+Methods to create arrays
+const arr1 = ["tset", 4, { hello: "hi htere" }];
+
+
+console.log(arr1);
+
+const arr2 = Array("hello", "God damn it", 57);
+
+const arr3 = new Array(5);
+
+arr3[1] = "tst";
+arr3.push(1);
+arr3.push(3);
+arr3.push(3);
+arr3.push(3);
+arr3.push(3);
+arr3.push(3);
+arr3.push(3);
+arr3.push(3);
+arr3.push(3);
+
+console.log(arr3);
+
+const arr1 = Array.from(["tets"]);
+console.log(arr1);
+
+const nodeList = document.querySelectorAll("li");
+console.log(nodeList);
+const listArray = Array.from(nodeList);
+console.log(listArray);
+
+*/
+
+/*
+Method to manipulate the array
+
+const arr = ["test", 2, "hello", 6];
+console.log(arr);
+
+const val = arr.pop();
+console.log(val);
+console.log(arr);
+arr.push("hey");
+console.log(arr);
+
+arr.unshift("General");
+console.log(arr);
+
+arr[1] = "Kenobi";
+console.log(arr);
+
+const name = arr.shift();
+const surname = arr.shift();
+
+console.log(`${name}  ${surname}`);
+
+console.log(arr);
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr1);
+
+arr1.splice(0, 0, 21, 24);
+
+console.log(arr1);
+
+let deletedElements = arr1.splice(2, 5);
+console.log(deletedElements);
+console.log(arr1);
+
+
+console.log(deletedElements);
+
+
+console.log(arr1);
+
+
+const arr1 = [2, 45, 4.33, 673, 8, 3];
+console.log(arr1);
+
+const copy = arr1.slice();
+
+console.log(copy);
+
+const arr1 = ["hello", "there"];
+const kenobi = ["General", "Kenobi"];
+arr5 = arr1.slice();
+
+const arr2 = arr1.concat(kenobi);
+
+console.log(arr2);
+
+arr1.push(kenobi);
+
+console.log(arr1);
+
+arr5.push(...kenobi);
+console.log(arr5);
+
+*/
