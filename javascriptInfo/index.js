ddcd2bfa4edec4df51ab6d56cf52f565f918cef4
Prateek Parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-07-22 17:57:10 +0530

    String exercises and chapter done

--- javascriptInfo/index.js
+++ javascriptInfo/index.js
@@ -1,3 +1,6 @@
+/* 
+String methods and exercises 
+
 console.log("Hello");
 
 const chadBacktick = `God damn it mate
@@ -20,7 +23,6 @@ const occuranceArray = (str, value) => {
     const result = [];
     let index = 0;
     while (str.indexOf(value, index) >= 0) {
-        console.log(`${value} ${index}`);
         let foundIndex = str.indexOf(value, index);
         result.push(foundIndex);
         console.log(result);
@@ -37,3 +39,40 @@ let y = 8;
 x = ++y;
 
 console.log(x);
+
+const getCharString = () => {
+    let result = "";
+    for (let i = 65; i < 150; i++) {
+        result += String.fromCodePoint(i);
+    }
+
+    return result;
+};
+
+console.log(getCharString());
+
+console.log(String.fromCharCode(61));
+
+console.log("A".codePointAt());
+
+console.log(Math.pow(2, 16));
+
+console.log("hello".length);
+
+console.log("tset".toUpperCase());
+
+const checkSpam = (str) => {
+    const problem = "XXX";
+    const prob = "VIAGRA";
+
+    strUpper = str.toUpperCase();
+
+    if (strUpper.includes(prob) || strUpper.includes(problem)) {
+        return true;
+    }
+    return false;
+};
+
+console.log("testthis".slice(0, 3));
+
+*/
