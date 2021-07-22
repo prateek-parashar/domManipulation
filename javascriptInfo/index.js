d67ff1f13546d5317807545e4fa6cfd143a67413
Prateek Parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-07-23 00:58:30 +0530

    Array Exercises from the bool

--- javascriptInfo/index.js
+++ javascriptInfo/index.js
@@ -116,3 +116,76 @@ let v = arr5.find((e) => e === "*");
 console.log(v);
 
 */
+
+/*
+
+Array exercises in the book
+
+const arr = [1, 5, 83, 9, 0, -1, 0.33];
+
+arr.sort((a, b) => a - b);
+
+console.log(arr);
+
+arr.sort((a, b) => b - a);
+console.log(arr);
+
+console.log(arr.filter((e) => e === 83));
+
+arr.forEach((e) => console.log(e));
+
+let arwr = ["HTML", "JavaScript", "CSS"];
+
+const copyArray = (arr) => {
+    let copy = arr.slice(0);
+
+    return copy.sort();
+};
+
+console.log(copyArray(arwr));
+
+console.log(arwr);
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [john, pete, mary];
+
+let usersMapped = users.map((user) => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id,
+}));
+
+console.log(usersMapped);
+console.log(usersMapped[0].id);
+console.log(usersMapped[0].fullName);
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+
+const unique = (arr) => {
+    let result = [];
+    console.log(arr.length);
+    for (let i = 0; i < 9; i++) {
+        console.log(i);
+        val = arr.pop();
+        console.log(arr);
+        if (!arr.includes(val)) {
+            result.push(val);
+        }
+    }
+
+    return result;
+};
+
+console.log(unique(strings));
+
+console.log(strings);
+
+*/
