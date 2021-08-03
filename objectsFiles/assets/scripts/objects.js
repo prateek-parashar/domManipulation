1512f5c5a8a1176be2a6d46a93cd35aa8eca4591
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-08-03 21:26:28 +0530

    Add the filter and render the filtered elements to the dom

--- objectsFiles/assets/scripts/objects.js
+++ objectsFiles/assets/scripts/objects.js
@@ -21,10 +21,34 @@ const addMovieHandler = () => {
 
     movieArray.push(movie);
 
-    console.log(movieArray);
-    if (movieArray.length > 0) {
+    const listElm = document.createElement("li");
+
+    listElm.innerText = movie.info.title;
+
+    movieList.append(listElm);
+    renderMovieList();
+};
+
+const renderMovieList = (arr = movieArray) => {
+    console.log(arr);
+    if (arr.length > 0) {
         movieList.classList.add("visible");
     }
+
+    movieList.innerHTML = "";
+
+    arr.forEach((element) => {
+        const listElm = document.createElement("li");
+        listElm.innerText = element.info.title;
+        movieList.append(listElm);
+    });
+};
+
+const searchBtnHandler = () => {
+    const pattern = document.getElementById("filter-title").value;
+    const filteredArr = movieArray.filter((e) => e.info.title.includes(pattern));
+    renderMovieList(filteredArr);
 };
 
 addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchBtnHandler);
