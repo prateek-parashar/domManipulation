8faece4c5facdff901ef556a6e8a726561820032
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-08-12 16:59:32 +0530

    Toggle methods for the modal windows

--- modalWindow/script.js
+++ modalWindow/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+const showModalHandler = () => {
+    const modal = document.querySelector(".modal");
+    const overlay = document.querySelector(".overlay");
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+};
+
+const buttonList = document.querySelectorAll("button");
+
+for (let btn of buttonList) {
+    btn.addEventListener("click", showModalHandler);
+}
+
+const overLay = document.querySelector(".overlay");
+
+overLay.addEventListener("click", showModalHandler);
