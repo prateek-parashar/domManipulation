84dc19358d5346a7db1e6c2466475f336aadf4bc
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-08-12 15:48:46 +0530

    Created the game logic, only thing remaining is the reset functionality

--- GuessNumber/script.js
+++ GuessNumber/script.js
@@ -1,1 +1,59 @@
-'use strict';
+"use strict";
+
+const SUCCESS_TEXT = "Congratulations! You got it ";
+
+const checkButton = document.querySelector(".btn.check");
+
+let secretNumber = 0;
+
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+const validateInput = (input, target) => {
+    if (input < 0 || input > 20) {
+        alert("Read the instructions dude");
+        return { result: false, hint: "Invalid input" };
+    }
+
+    if (input === target) {
+        return { result: true };
+    }
+    if (input < target) {
+        return { result: false, hint: "Too low" };
+    }
+
+    return { result: false, hint: "Too high" };
+};
+
+const checkButtonHandler = (target) => {
+    const userInput = +document.querySelector(".guess").value;
+    const output = validateInput(userInput, target);
+
+    if (output.result) {
+        document.querySelector(".message").textContent = SUCCESS_TEXT;
+        document.querySelector(".number").textContent = secretNumber;
+        document.body.style.backgroundColor = "green";
+        let currrentScore = +document.querySelector(".score").textContent;
+        let highScore = +document.querySelector(".highscore").textContent;
+
+        if (currrentScore > highScore) {
+            highScore = currrentScore;
+        }
+
+        document.querySelector(".highscore").textContent = highScore;
+    } else {
+        document.querySelector(".message").textContent = output.hint;
+
+        document.querySelector(".score").textContent -= 1;
+    }
+};
+
+const init = () => {
+    secretNumber = getRandomNumber(0, 20);
+    checkButton.addEventListener("click", () => {
+        checkButtonHandler(secretNumber);
+    });
+};
+
+init();
