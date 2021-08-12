bf5efa728a4f575e9ef556efdab0b17b051ad7da
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-08-13 01:38:09 +0530

    Initial selectors for the game dom

--- JSGame/script.js
+++ JSGame/script.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+const player1 = {
+    currentScore: 0,
+    currentScoreText: document.getElementById("current--0"),
+    totalScore: 0,
+    totalScoreText: document.getElementById("score--0"),
+    isActive: true,
+};
+
+const player2 = {
+    currentScore: 0,
+    currentScoreText: document.getElementById("current--1"),
+    totalScore: 0,
+    totalScoreText: document.getElementById("score--1"),
+    isActive: true,
+};
+
+const rollDiceBtn = document.querySelector("btn-roll");
+const holdBtn = document.querySelector("btn-hold");
+const newGameBtn = document.querySelector("btn-new");
+
+const init = () => {
+
+}
