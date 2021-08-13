c713a45e7db602cf9fca77affd9d35b7fe591b81
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2021-08-14 01:03:16 +0530

    Common methods to create and append elements to the DOM

--- advancedDom/script.js
+++ advancedDom/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-const openModal = function () {
+const openModal = function (e) {
+    e.preventDefault();
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
@@ -18,7 +19,9 @@ const closeModal = function () {
     overlay.classList.add("hidden");
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].addEventListener("click", openModal);
+for (const elem of btnsOpenModal) {
+    elem.addEventListener("click", openModal);
+}
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
@@ -27,4 +30,28 @@ document.addEventListener("keydown", function (e) {
     if (e.key === "Escape" && !modal.classList.contains("hidden")) {
         closeModal();
     }
+});
+
+// Testing the dom manipulation techniques
+
+// Selecting elements
+const header = document.querySelector(".header");
+
+const cookieAlert = document.createElement("div");
+
+cookieAlert.textContent = `Click me if you don't want cookies!`;
+cookieAlert.className = "cookie-message";
+
+const cookieConfirmButton = document.createElement("button");
+cookieConfirmButton.innerText = "Hell yeah Bitch";
+cookieConfirmButton.className = "btn";
+
+cookieAlert.insertAdjacentElement("beforeend", cookieConfirmButton);
+
+header.append(cookieAlert);
+
+header.before(cookieAlert.cloneNode(true));
+
+cookieConfirmButton.addEventListener("click", () => {
+    cookieAlert.remove();
 });
